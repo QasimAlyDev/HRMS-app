362203542c43b54e352de8dc2cf6ffc3545c95ee
QasimAlyDev <qasimaly18@gmail.com>
2024-07-13 12:03:16 +0500

    "Added email uniqueness check on registration form"

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -29,58 +29,64 @@
                     {{ csrf_field() }}
 
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Name" name="name" required value="{{ old('name') }}">
+                        <input type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Name"
+                            name="name" required value="{{ old('name') }}">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-user"></span>
                             </div>
                         </div>
                         @error('name')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
                         @enderror
                     </div>
 
                     <div class="input-group mb-3">
-                        <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email" required value="{{ old('email') }}">
+                        <input type="email" class="form-control @error('email') is-invalid @enderror"
+                            placeholder="Email" name="email" required value="{{ old('email') }}"
+                            onblur="duplicateEmail(this)">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
                             </div>
                         </div>
+                        <div class="invalid-feedback duplicate_message"></div>
                         @error('email')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
                         @enderror
                     </div>
 
                     <div class="input-group mb-3">
-                        <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password">
+                        <input type="password" class="form-control @error('password') is-invalid @enderror"
+                            placeholder="Password" name="password">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
                             </div>
                         </div>
                         @error('password')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
                         @enderror
                     </div>
 
                     <div class="input-group mb-3">
-                        <input type="password" class="form-control @error('confirm_password') is-invalid @enderror" placeholder="Confirm Password" name="confirm_password">
+                        <input type="password" class="form-control @error('confirm_password') is-invalid @enderror"
+                            placeholder="Confirm Password" name="confirm_password">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
                             </div>
                         </div>
                         @error('confirm_password')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
                         @enderror
                     </div>
 
@@ -98,12 +104,15 @@
                 </form>
                 <div class="d-flex justify-content-between mt-3">
                     <p class="mb-0">
-                        <a href="{{ url('/') }}" class="text-center" onmouseover="this.style.textDecoration='underline';" onmouseout="this.style.textDecoration='none';">
+                        <a href="{{ url('/') }}" class="text-center"
+                            onmouseover="this.style.textDecoration='underline';"
+                            onmouseout="this.style.textDecoration='none';">
                             Sign In
                         </a>
                     </p>
                     <p class="mb-1">
-                        <a href="{{ url('forgot-password') }}" onmouseover="this.style.textDecoration='underline';" onmouseout="this.style.textDecoration='none';">
+                        <a href="{{ url('forgot-password') }}" onmouseover="this.style.textDecoration='underline';"
+                            onmouseout="this.style.textDecoration='none';">
                             Forgot Password
                         </a>
                     </p>
@@ -120,8 +129,34 @@
     <!-- AdminLTE App -->
     <script src="{{ asset('backend/dist/js/adminlte.min.js') }}"></script>
     <!-- form validation , clear errors -->
-    <script src="{{ asset('backend/dist/js/customJS/removeError.js') }}">
-        
+    <script src="{{ asset('backend/dist/js/customJS/removeError.js') }}"></script>
+
+    <script type="text/javascript">
+        function duplicateEmail(element){
+            var email = $(element).val();
+            $.ajax({
+                type : "POST",
+                url  : '{{ url('check-email') }}',
+                data : {
+                        email : email,
+                        _token: "{{ csrf_token() }}"
+                    },
+                    dataType: "json",
+                    success:function(res){
+                        var errorMessage = $(element).closest('.input-group').find('.duplicate_message');
+                        if(res.exists){
+                            errorMessage.html('That email is taken. Try another.').show();
+                            $(element).addClass('is-invalid');
+                        } else {
+                            errorMessage.html('').hide();
+                            $(element).removeClass('is-invalid');
+                        }
+                    },
+                    error:function(jqXHR, exception){
+                        console.log("AJAX error: " + exception);
+                    }
+            })
+        }
     </script>
 </body>
 
