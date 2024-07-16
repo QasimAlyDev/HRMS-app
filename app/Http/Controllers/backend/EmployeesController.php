e128acf6cc7f464675832bb0fd8b43c729523dfa
qasimali <qasimaly18@gmail.com>
2024-07-16 23:32:26 +0500

    "Added validation to EmployeesController and updated add.blade.php with error handling and JavaScript to clear validation errors on input focus."

--- app/Http/Controllers/backend/EmployeesController.php
+++ app/Http/Controllers/backend/EmployeesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class EmployeesController extends Controller
@@ -17,6 +18,31 @@ class EmployeesController extends Controller
     }
     public function addEmployee_post(Request $request)
     {
-        dd($request->all());
+        $user = request()->validate([
+            'name'              => 'required',
+            'email'             => 'required|email|unique:users,email',
+            'hire_date'         => 'required',
+            'job_id'            => 'required',
+            'salary'            => 'required',
+            'commission_pct'    => 'required',
+            'manager_id'        => 'required',
+            'department_id'     => 'required'
+        ]);
+
+        $user                   = new User;
+        $user->name             = trim($request->name);
+        $user->last_name        = trim($request->last_name);
+        $user->email            = trim($request->email);
+        $user->phone_number     = trim($request->phone_number);
+        $user->hire_date        = trim($request->hire_date);
+        $user->job_id           = trim($request->job_id);
+        $user->salary           = trim($request->salary);
+        $user->commission_pct   = trim($request->commission_pct);
+        $user->manager_id       = trim($request->manager_id);
+        $user->department_id    = trim($request->department_id);
+        $user->is_role          = 0; // 0 - Employees
+        $user->save();
+
+        return redirect('admin/employees')->with('success' , 'Employee Successfully Resgister.');
     }
 }
--- resources/views/backend/employees/add.blade.php
+++ resources/views/backend/employees/add.blade.php
@@ -29,8 +29,7 @@
                         <div class="card-header">
                             <h3 class="card-title">Add Employee</h3>
                         </div>
-                        <form class="form-horizontal" method="post" accept="{{ url('admin/employees/add') }}" 
-                        action="" enctype="multipart/form-data">
+                        <form class="form-horizontal" method="post" accept="{{ url('admin/employees/add') }}" enctype="multipart/form-data">
 
                         @csrf
 
@@ -38,95 +37,135 @@
                             <div class="form-group row">
                                 <label for="name" class="col-sm-2 col-form-label">
                                     First Name <span style="color:red;">*</span></label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" name="name"
-                                        placeholder="Enter First Name" value="{{old('name')}}" required>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+                                    placeholder="Enter First Name" value="{{old('name')}}" required>
+                                    @error('name')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
                                     </div>
+                                    @enderror
+                                </div>
                             </div>        
                             <div class="form-group row">
                                 <label for="last_name" class="col-sm-2 col-form-label">Last Name</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" name="last_name"
-                                        placeholder="Enter Last Name" value="{{old('last_name')}}">
-                                    </div>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" name="last_name"
+                                    placeholder="Enter Last Name" value="{{old('last_name')}}">
+                                </div>
                             </div>        
                             <div class="form-group row">
                                 <label for="email" class="col-sm-2 col-form-label">
                                     Email <span style="color:red;">*</span></label>
-                                    <div class="col-sm-10">
-                                        <input type="email" class="form-control" name="email"
-                                        placeholder="Enter Email ID" value="{{old('email')}}" required>
+                                <div class="col-sm-10">
+                                    <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                                    placeholder="Enter Email ID" value="{{old('email')}}" required>
+                                    @error('email')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
                                     </div>
+                                    @enderror
+                                </div>
                             </div>        
                             <div class="form-group row">
-                                <label for="number" class="col-sm-2 col-form-label">Phone Number</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" name="phone_number"
-                                        placeholder="Enter Phone Number" value="{{old('phone_number')}}">
-                                    </div>
+                                <label for="phone_number" class="col-sm-2 col-form-label">Phone Number</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" name="phone_number"
+                                    placeholder="Enter Phone Number" value="{{old('phone_number')}}">
+                                </div>
                             </div>        
                             <div class="form-group row">
                                 <label for="hire_date" class="col-sm-2 col-form-label">Hire Date
                                     <span style="color:red;">*</span>
                                 </label>
-                                    <div class="col-sm-10">
-                                        <input type="date" class="form-control" name="hire_date"
-                                            value="{{old('hire_date')}}" required>
+                                <div class="col-sm-10">
+                                    <input type="date" class="form-control @error('hire_date') is-invalid @enderror" name="hire_date"
+                                        value="{{old('hire_date')}}" required>
+                                    @error('hire_date')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
                                     </div>
+                                    @enderror
+                                </div>
                             </div>        
                             <div class="form-group row">
-                                <label for="Job Title" class="col-sm-2 col-form-label">Job Title
+                                <label for="job_id" class="col-sm-2 col-form-label">Job Title
                                     <span style="color:red;">*</span>
                                 </label>
-                                    <div class="col-sm-10">
-                                        <select name="job_id" class="form-control" required>
-                                            <option value="">Select Job</option>
-                                            <option value="1">Web Developer</option>
-                                            <option value="2">PDF Developer</option>
-                                        </select>
+                                <div class="col-sm-10">
+                                    <select name="job_id" class="form-control @error('job_id') is-invalid @enderror" required>
+                                        <option value="">Select Job</option>
+                                        <option value="1">Web Developer</option>
+                                        <option value="2">PDF Developer</option>
+                                    </select>
+                                    @error('job_id')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
                                     </div>
+                                    @enderror
+                                </div>
                             </div>   
                             <div class="form-group row">
                                 <label for="salary" class="col-sm-2 col-form-label">Salary
                                     <span style="color:red;">*</span>
                                 </label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" name="salary"
-                                        placeholder="Enter Salary" value="{{old('salary')}}" required>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control @error('salary') is-invalid @enderror" name="salary"
+                                    placeholder="Enter Salary" value="{{old('salary')}}" required>
+                                    @error('salary')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
                                     </div>
+                                    @enderror
+                                </div>
                             </div>       
                             <div class="form-group row">
-                                <label for="Commission Pct" class="col-sm-2 col-form-label">Commission PCT
+                                <label for="commission_pct" class="col-sm-2 col-form-label">Commission PCT
                                     <span style="color:red;">*</span>
                                 </label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" name="commission_pct"
-                                        placeholder="Enter Commission PCT" value="{{old('commission_pct')}}" required>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control @error('commission_pct') is-invalid @enderror" name="commission_pct"
+                                    placeholder="Enter Commission PCT" value="{{old('commission_pct')}}" required>
+                                    @error('commission_pct')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
                                     </div>
+                                    @enderror
+                                </div>
                             </div>
                             <div class="form-group row">
-                                <label for="Manager Name" class="col-sm-2 col-form-label">Manager Name
+                                <label for="manager_id" class="col-sm-2 col-form-label">Manager Name
                                     <span style="color:red;">*</span>
                                 </label>
-                                    <div class="col-sm-10">
-                                        <select name="manager_id" class="form-control" required>
-                                            <option value="">Select Manager Name</option>
-                                            <option value="1">abc</option>
-                                            <option value="2">xyz</option>
-                                        </select>
+                                <div class="col-sm-10">
+                                    <select name="manager_id" class="form-control @error('manager_id') is-invalid @enderror" required>
+                                        <option value="">Select Manager Name</option>
+                                        <option value="1">abc</option>
+                                        <option value="2">xyz</option>
+                                    </select>
+                                    @error('manager_id')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
                                     </div>
+                                    @enderror
+                                </div>
                             </div> 
                             <div class="form-group row">
-                                <label for="Department Name" class="col-sm-2 col-form-label">Department Name
+                                <label for="department_id" class="col-sm-2 col-form-label">Department Name
                                     <span style="color:red;">*</span>
                                 </label>
-                                    <div class="col-sm-10">
-                                        <select name="department_it" class="form-control" required>
-                                            <option value="">Select Department Name</option>
-                                            <option value="1">Developer Department</option>
-                                            <option value="2">BDM Department</option>
-                                        </select>
+                                <div class="col-sm-10">
+                                    <select name="department_id" class="form-control @error('department_id') is-invalid @enderror" required>
+                                        <option value="">Select Department Name</option>
+                                        <option value="1">Developer Department</option>
+                                        <option value="2">BDM Department</option>
+                                    </select>
+                                    @error('department_id')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
                                     </div>
+                                    @enderror
+                                </div>
                             </div>   
                             <div class="card-footer">
                                 <a href="{{ url('admin/employees') }}" class="btn btn-default">Back</a>
@@ -142,5 +181,18 @@
     </section>
 </div>
 <!-- /.content-wrapper -->
-
-@endsection   
+@endsection  
+@section('script')
+<script type="text/javascript">
+    $(document).ready(function () {
+        // Clear validation errors on input focus
+        $('.form-control').on('input', function () {
+            var input = $(this);
+            if (input.val()) {
+                input.removeClass('is-invalid');
+                input.siblings('.invalid-feedback').hide();
+            } 
+        });
+    });
+</script>
+@endsection 
