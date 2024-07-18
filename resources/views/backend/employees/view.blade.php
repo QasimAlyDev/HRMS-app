324892fb1812725f5ee7460288c4429620e03265
QasimAlyDev <qasimaly18@gmail.com>
2024-07-18 16:49:41 +0500

    "Refactored view blade templates for employees and jobs, updating layout and formatting."

--- resources/views/backend/employees/view.blade.php
+++ resources/views/backend/employees/view.blade.php
@@ -1,147 +1,94 @@
 @extends('backend.layouts.app')
 
 @section('content')
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">View Employees</h1>
-                    </div><!-- /.col -->
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">View</a></li>
-                            <li class="breadcrumb-item active">Employees</li>
-                        </ol>
-                    </div><!-- /.col -->
-                </div><!-- /.row -->
-            </div><!-- /.container-fluid -->
-        </div>
-        <!-- /.content-header -->
-        <section class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    <section class="col-lg-12">
-                        <div class="card card-info">
-                            <div class="card-header">
-                                <h3 class="card-title">View Employee</h3>
-                            </div>
-                            <form class="form-horizontal" enctype="multipart/form-data">
-                                <div class="card-body">
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            ID</label>
-                                        <div class="col-sm-10">
-                                            {{ $getRecord->id }}
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            First Name </label>
-                                        <div class="col-sm-10">
-                                            {{ $getRecord->name }}
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            Last Name </label>
-                                        <div class="col-sm-10">
-                                            {!! !empty($getRecord->last_name) ? $getRecord->last_name : '<p class="text-danger">No Available</p>' !!}
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            Email </label>
-                                        <div class="col-sm-10">
-                                            {{ $getRecord->email }}
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            Phone Number </label>
-                                        <div class="col-sm-10">
-                                            {!! !empty($getRecord->phone_number) ? $getRecord->phone_number : '<p class="text-danger">No Available</p>' !!}
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            Hire Date </label>
-                                        <div class="col-sm-10">
-                                            {!! !empty($getRecord->hire_date) ? date('d-m-Y' , strtotime($getRecord->hire_date)) : '<p class="text-danger">No Available</p>' !!}
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            Job Title </label>
-                                        <div class="col-sm-10">
-                                            {!! !empty($getRecord->job_id) ? $getRecord->job_id : '<p class="text-danger">No Available</p>' !!}
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            Salary </label>
-                                        <div class="col-sm-10">
-                                            {!! !empty($getRecord->salary) ? $getRecord->salary : '<p class="text-danger">No Available</p>' !!}
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            Commission PCT </label>
-                                        <div class="col-sm-10">
-                                            {!! !empty($getRecord->commission_pct) ? $getRecord->commission_pct : '<p class="text-danger">No Available</p>' !!}
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            Manager Name </label>
-                                        <div class="col-sm-10">
-                                            {!! !empty($getRecord->manager_id) ? $getRecord->manager_id : '<p class="text-danger">No Available</p>' !!}
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            Department Name </label>
-                                        <div class="col-sm-10">
-                                            {!! !empty($getRecord->department_id) ? $getRecord->department_id : '<p class="text-danger">No Available</p>' !!}
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            Is Role </label>
-                                        <div class="col-sm-10">
-                                            {!! !empty($getRecord->is_role) ? 'HR' : 'Employee' !!}
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            Created Date </label>
-                                        <div class="col-sm-10">
-                                            {!! !empty($getRecord->created_at) ? date('d-m-Y H:i A' , strtotime($getRecord->created_at)) : '<p class="text-danger">No Available</p>' !!}
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            Updated Date </label>
-                                        <div class="col-sm-10">
-                                            {!! !empty($getRecord->updated_at) ? date('d-m-Y H:i A' , strtotime($getRecord->updated_at)) : '<p class="text-danger">No Available</p>' !!}
-                                        </div>
-                                    </div>
-
-                                </div>
-                                <div class="card-footer">
-                                    <a href="{{ url('admin/employees') }}" class="btn btn-default">Back</a>
-                                </div>
-                            </form>
-                        </div>
-
-                    </section>
+<div class="content-wrapper">
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">View Employees</h1>
                 </div>
-
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">View</a></li>
+                        <li class="breadcrumb-item active">Employees</li>
+                    </ol>
+                </div>
             </div>
-        </section>
-
+        </div>
     </div>
-    <!-- /.content-wrapper -->
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <section class="col-lg-12">
+                    <div class="card card-info">
+                        <div class="card-header">
+                            <h3 class="card-title">View Employee</h3>
+                        </div>
+                        <form class="form-horizontal" enctype="multipart/form-data">
+                            <div class="card-body">
+                                <div class="row mb-3">
+                                    <div class="form-group col-md-4 border">
+                                        <label>ID</label> : {{ $getRecord->id }}
+                                    </div>
+                                    <div class="form-group col-md-4 border">
+                                        <label>First Name</label> : {{ $getRecord->name }}
+                                    </div>
+                                    <div class="form-group col-md-4 border">
+                                        <label>Last Name</label> : {!! !empty($getRecord->last_name) ? $getRecord->last_name : '<span class="text-danger">No Available</span>' !!}
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="form-group col-md-4 border">
+                                        <label>Email</label> : {{ $getRecord->email }}
+                                    </div>
+                                    <div class="form-group col-md-4 border">
+                                        <label>Phone Number</label> : {!! !empty($getRecord->phone_number) ? $getRecord->phone_number : '<span class="text-danger">No Available</span>' !!}
+                                    </div>
+                                    <div class="form-group col-md-4 border">
+                                        <label>Hire Date</label> : {!! !empty($getRecord->hire_date) ? date('d-m-Y', strtotime($getRecord->hire_date)) : '<span class="text-danger">No Available</span>' !!}
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="form-group col-md-4 border">
+                                        <label>Job Title</label> : {!! !empty($getRecord->job_id) ? $getRecord->job_id : '<span class="text-danger">No Available</span>' !!}
+                                    </div>
+                                    <div class="form-group col-md-4 border">
+                                        <label>Salary</label> : {!! !empty($getRecord->salary) ? $getRecord->salary : '<span class="text-danger">No Available</span>' !!}
+                                    </div>
+                                    <div class="form-group col-md-4 border">
+                                        <label>Commission PCT</label> : {!! !empty($getRecord->commission_pct) ? $getRecord->commission_pct : '<span class="text-danger">No Available</span>' !!}
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="form-group col-md-4 border">
+                                        <label>Manager Name</label> : {!! !empty($getRecord->manager_id) ? $getRecord->manager_id : '<span class="text-danger">No Available</span>' !!}
+                                    </div>
+                                    <div class="form-group col-md-4 border">
+                                        <label>Department Name</label> : {!! !empty($getRecord->department_id) ? $getRecord->department_id : '<span class="text-danger">No Available</span>' !!}
+                                    </div>
+                                    <div class="form-group col-md-4 border">
+                                        <label>Is Role</label> : {!! !empty($getRecord->is_role) ? 'HR' : 'Employee' !!}
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="form-group col-md-4 border">
+                                        <label>Created Date</label> : {!! !empty($getRecord->created_at) ? date('d-m-Y H:i A', strtotime($getRecord->created_at)) : '<span class="text-danger">No Available</span>' !!}
+                                    </div>
+                                    <div class="form-group col-md-4 border">
+                                        <label>Updated Date</label> : {!! !empty($getRecord->updated_at) ? date('d-m-Y H:i A', strtotime($getRecord->updated_at)) : '<span class="text-danger">No Available</span>' !!}
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <a href="{{ url('admin/employees') }}" class="btn btn-default">Back</a>
+                            </div>
+                        </form>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </section>
+</div>
 @endsection
--- resources/views/backend/jobs/view.blade.php
+++ resources/views/backend/jobs/view.blade.php
@@ -1,91 +1,72 @@
 @extends('backend.layouts.app')
 
 @section('content')
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">View Jobs</h1>
-                    </div><!-- /.col -->
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">View</a></li>
-                            <li class="breadcrumb-item active">Jobs</li>
-                        </ol>
-                    </div><!-- /.col -->
-                </div><!-- /.row -->
-            </div><!-- /.container-fluid -->
-        </div>
-        <!-- /.content-header -->
-        <section class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    <section class="col-lg-12">
-                        <div class="card card-info">
-                            <div class="card-header">
-                                <h3 class="card-title">View Job</h3>
-                            </div>
-                            <form class="form-horizontal" enctype="multipart/form-data">
-                                <div class="card-body">
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            ID</label>
-                                        <div class="col-sm-10">
-                                            {{ $getRecord->id }}
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            Job Title </label>
-                                        <div class="col-sm-10">
-                                            {{ $getRecord->job_title }}
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            Min Salary </label>
-                                        <div class="col-sm-10">
-                                            {{ $getRecord->min_salary }}
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            Max Salary </label>
-                                        <div class="col-sm-10">
-                                            {{ $getRecord->max_salary }}
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            Created Date </label>
-                                        <div class="col-sm-10">
-                                            {{ date('d-m-Y H:i A' , strtotime($getRecord->created_at)) }}
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">
-                                            Updated Date </label>
-                                        <div class="col-sm-10">
-                                            {{ date('d-m-Y H:i A' , strtotime($getRecord->updated_at)) }}
-                                        </div>
-                                    </div>
-
-                                </div>
-                                <div class="card-footer">
-                                    <a href="{{ url('admin/jobs') }}" class="btn btn-default">Back</a>
-                                </div>
-                            </form>
-                        </div>
-
-                    </section>
-                </div>
-
-            </div>
-        </section>
-
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">View Jobs</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">View</a></li>
+                        <li class="breadcrumb-item active">Jobs</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
     </div>
-    <!-- /.content-wrapper -->
+    <!-- /.content-header -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <section class="col-lg-12">
+                    <div class="card card-info">
+                        <div class="card-header">
+                            <h3 class="card-title">View Job</h3>
+                        </div>
+                        <form class="form-horizontal" enctype="multipart/form-data">
+                            <div class="card-body">
+                                <div class="row mb-3">
+                                    <div class="form-group col-md-4 border">
+                                        <label> ID</label> : {{ $getRecord->id }}
+                                    </div>
+                                    <div class="form-group col-md-4 border">
+                                        <label> Job Title </label> : {{ $getRecord->job_title }}
+                                    </div>
+                                    <div class="form-group col-md-4 border">
+                                        <label> Min Salary </label> : {{ $getRecord->min_salary }}
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="form-group col-md-4 border">
+                                        <label> Max Salary </label> : {{ $getRecord->max_salary }}
+                                    </div>
+                                    <div class="form-group col-md-4 border">
+                                        <label> Created Date </label> : {{ date('d-m-Y H:i A' ,
+                                        strtotime($getRecord->created_at)) }}
+                                    </div>
+                                    <div class="form-group col-md-4 border">
+                                        <label> Updated Date </label> : {{ date('d-m-Y H:i A' ,
+                                        strtotime($getRecord->updated_at)) }}
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <a href="{{ url('admin/jobs') }}" class="btn btn-default">Back</a>
+                            </div>
+                        </form>
+                    </div>
+
+                </section>
+            </div>
+
+        </div>
+    </section>
+
+</div>
+<!-- /.content-wrapper -->
 @endsection
